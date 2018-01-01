ed2006b811b2819fbd5c69598c78af711b1ca8e3
TJ Stubbs <stubbsrule@gmail.com>
2018-01-01 14:20:03 -0500

    created db and portfolio table

--- portfolio/db/migrations/migrations12292017.sql
+++
@@ -1,7 +0,0 @@
--- /c portfolio_auth_dev
---
--- CREATE TABLE IF NOT EXISTS portfolio (
---   id BIGSERIAL PRIMARY KEY,
---   title VARCHAR(255),
---   description TEXT
--- )
--- portfolio/db/seed/seed.sql
+++ portfolio/db/seed/seed.sql
@@ -1,12 +1,12 @@
 -- /c portfolio_auth_dev
--- INSERT INTO portfolio (title, description, image) VALUES
--- (
---   'Racer Game',
---   'Race your opponent in the race of the Ages',
---   'https://insertlinkhere'
--- )
--- (
---   'Simpsons Trivia Game',
---   'Test your Simpsons knowledge in this new age trivia game',
---   'https://insertlinkhere'
--- )
+INSERT INTO portfolio (title, description, image) VALUES
+(
+  'Racer Game',
+  'Race your opponent in the race of the Ages',
+  'https://res.cloudinary.com/dftzmialt/image/upload/v1512059497/Screen_Shot_2017-11-30_at_11.29.34_AM_xtoglf.png'
+),
+(
+  'Simpsons Trivia Game',
+  'Test your Simpsons knowledge in this new age trivia game',
+  'https://res.cloudinary.com/dftzmialt/image/upload/v1512673427/springfield_fgu8jt.jpg'
+)
